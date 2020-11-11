f064487859ec4ca7d44a9e35e08c44668fef9719
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2020-11-11 22:32:48 +0700

    feat: remove padding

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,12 @@ import HomeAndInputFragment from "../components/fragment/home-and-input";
 const Home = () => {
   return (
     <Box
-      p="0 1rem"
       bg="indigo01"
       height="100vh"
       display="flex"
       justifyContent="center"
     >
-      <Box bg="white" width="30em">
+      <Box bg="white" maxWidth="30em" width="100%">
         <HomeAndInputFragment />
       </Box>
     </Box>
